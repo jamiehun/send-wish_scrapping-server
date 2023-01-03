e8fcdc15e87e92155919da3f894bc3709b5989b5
jamiehun <jamiesunghun@gmail.com>
2023-01-03 22:31:02 +0900

    chore : before integration with rg

--- selenium_xpath/integration/coupang.py
+++ selenium_xpath/integration/coupang.py
@@ -31,7 +31,7 @@ driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 # URL (쿠팡, 네이버, 무신사 순)
 # url = 'https://m.coupang.com/vm/products/6552462584?itemId=14634004658&vendorItemId=81875544125' # 쿠팡
 url = "https://m.shopping.naver.com/kids/stores/1000016176/products/4847841465?NaPm=ct%3Dlcfs8ykw%7Cci%3D6ae6545058bf0a5b6906fdb0e42605ec5ab66f78%7Ctr%3Dbrc%7Csn%3D203038%7Chk%3Dafd41c17626461775eeac008bf5481009bea4150" # 네이버
-# url = "https://www.musinsa.com/app/goods/2926375?loc=goods_rank"
+# url = "https://www.musinsa.com/app/goods/2926375?loc=goods_rank" # 무신사
 
 driver.get(url)
 print(driver.get_window_size())
@@ -44,39 +44,85 @@ if (url.find("coupang.com") != -1):
     close_btn.click()
     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="pdpImages"]/ul/li[1]/img')))
 
-## xpath 활용하지 않고 들고오기 (쿠팡)
-# 1. 이미지
-# 2. 상품명
-# 3. 상품가격
-# 4. 할인가격 
 
-# 쿠팡일 경우
-if (url.find("coupang.com") != -1):
-    # 1. 이미지 스크래핑
-    img_collector = driver.find_element(By.ID, "pdpImages")
-    imgs = img_collector.find_elements(By.TAG_NAME, 'img')
+# ## 1. 이미지 스크래핑 == ##
+# # 쿠팡일 경우
+# if (url.find("coupang.com") != -1):
+#     # 1. 이미지 스크래핑
+#     img_collector = driver.find_element(By.ID, "pdpImages")
+#     imgs = img_collector.find_elements(By.TAG_NAME, 'img')
 
-# 그 외의 경우
-else:
-    # 1. 이미지 스크래핑
-    imgs = driver.find_elements(By.TAG_NAME, 'img')
+# # 그 외의 경우
+# else:
+#     # 1. 이미지 스크래핑
+#     imgs = driver.find_elements(By.TAG_NAME, 'img')
     
-    # 2. 텍스트 스크래핑 
-    content = driver.find_elements(By.ID, 'content')
-    for t in content:
-        if t.location['y'] <= window_height:
-            title = t.find_element(By.TAG_NAME, 'h3')
-            print('제목 : ', title.text)
+
+# for i in imgs:
+#     img = i.get_attribute("src")
+#     # print(img)
+#     if 136 < img.__sizeof__()<= 400:
+#         print("이미지: ", img)
+#         break
+    
+
+
+# ## 2. 텍스트 스크래핑 (meta 활용) ##
+# title_scrap = False
+
+# try:
+#     meta_title = driver.find_element(By.XPATH, "//meta[@property='og:title']")
+    
+#     if (meta_title != 0):
+#         title = meta_title
+#         print("제목 :", title.get_attribute("content"))
+#         title_scrap = True
+# except:
+#     pass
+
+
+# # 2-2. 텍스트 스크래핑 (클래스 활용)
+# if title_scrap == False:
+#     try:
+#         class_title = driver.find_elements(By.XPATH, "//*[contains(@class, 'title')]")
+        
+#         for t in class_title:
+#             if (t.text != "" and t.location['y'] <= window_height*1):
+#                 title = t.text
+#                 print("제목 :", title)
+#                 title_scrap = True
+#                 break
+
+#     except:
+#         pass
+    
+
+## 3. 가격 스크래핑 (meta 활용) ##
+# 1) window height 체크
+# 2) 텍스트 크기 체크해서
+price_scrap = False
+
+
+wons = driver.find_elements(By.XPATH, '//*[contains(@class, "price")]')
+for w in wons:
+    if (w.location['y'] <= window_height) and ("원" in w.text) and (w.text != ""):
+        print("가격: ", w.text)
+        price_scrap = True
+
+if (price_scrap == False) :
+    wons = driver.find_elements(By.XPATH, '//*[contains(@div, "원")]')
+    for w in wons:
+        if (w.location['y'] <= window_height) and ("원" in w.text) and (w.text != ""):
+            print("가격: ", w.text)
+
+
+# won = driver.find_element(By.XPATH, f'//*[contains(text(), "원")]/preceding-sibling::span').text
+
         
 
 
 
-for i in imgs:
-    img = i.get_attribute("src")
-    # print(img)
-    if 136 < img.__sizeof__()<= 400:
-        print("이미지: ", img)
-        break
+
 
 # 2. 이미지
 
